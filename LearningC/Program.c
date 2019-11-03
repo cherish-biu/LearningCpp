5ac5492c2b173fbe58bcd151cc0c4e094e0785d5
cherish-biu <995106721@qq.com>
2019-11-03 20:14:15 +0800

    Initial commit

---
+++ LearningC/Program.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#pragma warning(disable:4996)
+
+int main(void)
+{
+	int x;
+
+	printf("Please enter an integer:\n");
+	scanf("%d", &x);
+	printf("Double the integer you entered, we have: %d\n", 2 * x);
+
+	//system("pause");
+	return 0;
+}
